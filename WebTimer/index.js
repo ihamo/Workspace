cf8373fd53e9f9fb3ab53d8882c8ffd95b19f0c2
J0H4NO <iwanhamo@googlemail.com>
2021-06-28 11:16:38 +0200

    circle attribute

--- WebTimer/index.js
+++ WebTimer/index.js
@@ -1,14 +1,21 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+// Set Circle Attributes
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	onStart() {
 		console.log('Timer has started');
 	},
 	onTick() {
-		console.log('Timer ticked down');
-		console.log(this.timeRemaining);
+		currentOffset = currentOffset - 50;
+		circle.setAttribute('stroke-dashoffset', currentOffset);
 	},
 	onComplete() {
 		console.log('Timer is Complete');
