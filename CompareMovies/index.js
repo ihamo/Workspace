171b81bae96dc99f2fec1308a5a9209448c62861
J0H4NO <iwanhamo@googlemail.com>
2021-06-30 09:59:19 +0200

    Update index.js

--- CompareMovies/index.js
+++ CompareMovies/index.js
@@ -1,1 +1,17 @@
-console.log("hey there!")
+//TODO
+// Fetch Data about Movie -> API
+// build an autocomplete widget from scratch
+
+
+const fetchData = async () => {
+	// we will wait for the response
+	const response = await axios.get('http://www.omdbapi.com', {
+		params: {
+			apikey: '783e8d68',
+			s: 'avengers'
+		}
+	});
+	console.log(response.data);
+};
+
+fetchData();
