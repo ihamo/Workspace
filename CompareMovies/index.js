388ffab14325384c768d701020882c0a1a321a95
J0H4NO <iwanhamo@googlemail.com>
2021-07-06 12:17:28 +0200

    Update index.js

--- CompareMovies/index.js
+++ CompareMovies/index.js
@@ -1,4 +1,7 @@
-
+//TODO
+// Fetch Data about Movie -> API
+// build an autocomplete widget from scratch
+// API KEY: 783e8d68
 
 const fetchData = async (searchTerm) => {
 	// we will wait for the response
@@ -9,36 +12,21 @@ const fetchData = async (searchTerm) => {
 			s: searchTerm
 		}
 	});
-	console.log(response.data);
+	console.log(response.data.Search);
+	return response.data.Search;
 };
 
 const SearchInput = document.querySelector('#SearchInput');
 
-// If the user start tipping, the setTimeOut Method will wait 2 Sec before show the result
-// to prevent to many requests.
-// If the user start tipping the event occur and will set up a request, we will Kill the request if the User is still tipping
-
-const debounce = (func) => {
-	let timeOutId;
-	return (...args) => {
-		if (timeOutId) {
-			clearTimeout(timeOutId);
-		}
-		timeOutId = setTimeout(() => {
-			func.apply(null, args);
-		}, 2000);
-	};
-};
-
 // Event Function
-const onInput = debounce((event) => {
+const onInput = async (event) => {
 	// event.target.value -> input value
 	// pass the input value as an argument
-	fetchData(event.target.value);
-});
+	const Movies = await fetchData(event.target.value);
+};
 
 // EventListener, passed the Event Function
-SearchInput.addEventListener('input', onInput);
+SearchInput.addEventListener('input', debounce(onInput, 1000));
 
 // Same Function like above
 // const onInput = (event) => {
