accd4773e24dc1e768305b9a4468a575b07f8a7b
J0H4NO <iwanhamo@googlemail.com>
2021-07-28 17:14:20 +0200

    Update App.js

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,29 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Header from './components/Header';
 
 function App() {
-	// The first element in the array is the current State
-	// The second element is the function that update that current state
-	const [ count, setCount ] = useState(4);
-
-	function decrementCount() {
-		setCount(count - 1);
-	}
-
-	function incrementCount() {
-		setCount(count + 1);
-	}
-
 	return (
-		<div>
-			<button onClick={decrementCount}>-</button>
-			<span> {count} </span>
-			<button onClick={incrementCount}>+</button>
+		<div className="container">
+			<Header title={'React to React'} />
 		</div>
 	);
 }
 
-export default App;
+// Class based Component
 
-// Hooks
-// Hooks can only be used in Functions
-// React must be called in the exact same order in every component render
+// class App extends React.Component {
+// 	render() {
+// 		return <h1>Hello from a Class</h1>;
+// 	}
+// }
+
+export default App;
